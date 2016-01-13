a39f043a850b847062a70a9a469da3efb663ef79
Maximiliano Guzenski <max@guzenski.com.br>
2016-01-13 18:47:19 -0300

    #fix 2
    This #fix issue 2 and was added a test to prove it.

--- test/index.js
+++ test/index.js
@@ -102,6 +102,18 @@ describe('debounce middleware', () => {
     });
   });
 
+  describe('action is dispatched even has meta without debounce', () => {
+    beforeEach(() => store.dispatch({
+      type: 'SEARCH',
+      payload: 'foo',
+      meta: { other: 'other' }
+    }));
+
+    it('state is updated straight away', () => {
+      assert.deepEqual(store.getState(), {query:'foo'});
+    });
+  });
+
   describe('debounced action is fired many times', () => {
     const action = {
       type: 'UPDATE',
